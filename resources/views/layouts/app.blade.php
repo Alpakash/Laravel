136b11022353f0ee6f7e96c0827320248405410b
Tuan <s1131670@student.windesheim.nl>
2018-11-26 13:25:21 +0100

    md bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,11 @@
 
     <!-- Font awesome icons -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/css/mdb.min.css" />
+    <link rel="stylesheet" href="/css/style.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <!-- Bootstrap Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
@@ -102,7 +106,9 @@
             @yield('content')
         </main>
     </div>
-    
+
+
+    @include('layouts.scripts')
 
 
 </body>
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="/javascripts/jquery-3.3.1.min.js"></script>
+<script src="/javascripts/popper.min.js"></script>
+<script src="/javascripts/bootstrap.min.js"></script>
+<script src="/javascripts/mdb.min.js"></script>
+<script src="/javascripts/form-validate.js"></script>
+<script src="/javascripts/map.js"></script>
